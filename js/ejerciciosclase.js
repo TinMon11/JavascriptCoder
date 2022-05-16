957997c57957161face99ef18c71444d710f59a7
TinMon11 <martindanielmondino@gmail.com>
2022-05-16 16:39:36 -0300

    Se agrega array y funcion para agregar registros. Se simplifica funcion de ModificarSaldo.

--- js/ejerciciosclase.js
+++ js/ejerciciosclase.js
@@ -1,59 +1,102 @@
- class Producto {
+//  class Producto {
 
-     constructor(id,nombre,precio)
-     {
-     this.id=id;
-     this.nombre=nombre;
-     this.precio=precio;
+//      constructor(id,nombre,precio)
+//      {
+//      this.id=id;
+//      this.nombre=nombre;
+//      this.precio=precio;
+//  }
+//  PrecioConIVA()
+//   {
+//       return this.precio*1.21;
+//   }
+
+//  }
+
+//  class Persona {
+
+//      constructor(id,rol,nombre,apellido)
+//      {
+//          this.id=id;
+//          this.rol=rol;
+//          this.nombre=nombre;
+//          this.apellido=apellido;
+//      }
+
+//      Comprador()
+//      {
+//          return this.nombre + " " + this.apellido
+
+//      }
+//  }
+
+//  class Rol {
+
+//      constructor(id,nombre)
+//      {
+//          this.id=id;
+//          this.nombre=nombre;
+//      }
+//  }
+
+//  class Compra {
+
+//      constructor(producto,persona)
+//      {
+//          this.producto=producto;
+//          this.persona=persona;
+//      }
+//  }
+
+
+//  const producto = new Producto("1", "Arroz","100")
+//  const rol= new Rol("100","Cliente")
+//  const persona = new Persona("1020",rol,"Gonzalo","Perez")
+
+//  const compra = new Compra(producto,persona);
+
+//  console.log(compra)
+//  console.log(producto.PrecioConIVA())
+
+// const misNumeros = [1,3,5,6,"Hola",14]
+
+// //lenght para que te de la cnatida de elementos
+
+// alert(`El largo del array es ${misNumeros.length}`)
+
+//Push - para agregar valores al final del array --- UNSHIFT mete al prinicipio del array
+// POP quita el ultimo del array
+// 
+
+
+// misNumeros.push(104)
+
+
+
+// for (let i=0;i<misNumeros.length;i++) {
+//     alert(`Valor de la posicion ${i} es: ${misNumeros[i]}`)
+// }
+
+let i=0;
+
+const miArray = []
+
+while (i<5) {
+    let numero = Number(prompt(`Ingrese un numero:`));
+    miArray.push(numero);
+    i++;
+}
+
+alert(miArray)
+
+let suma = 0;
+let multip = 1;
+
+for (let i=0;i<miArray.length;i++) {
+
+      suma = suma + miArray[i];
+      multip = multip*miArray[i];
  }
- PrecioConIVA()
-  {
-      return this.precio*1.21;
-  }
 
- }
-
- class Persona {
-
-     constructor(id,rol,nombre,apellido)
-     {
-         this.id=id;
-         this.rol=rol;
-         this.nombre=nombre;
-         this.apellido=apellido;
-     }
-
-     Comprador()
-     {
-         return this.nombre + " " + this.apellido
-
-     }
- }
-
- class Rol {
-
-     constructor(id,nombre)
-     {
-         this.id=id;
-         this.nombre=nombre;
-     }
- }
-
- class Compra {
-
-     constructor(producto,persona)
-     {
-         this.producto=producto;
-         this.persona=persona;
-     }
- }
-
-
- const producto = new Producto("1", "Arroz","100")
- const rol= new Rol("100","Cliente")
- const persona = new Persona("1020",rol,"Gonzalo","Perez")
-
- const compra = new Compra(producto,persona);
-
- console.log(compra)
- console.log(producto.PrecioConIVA())
+ alert(suma)
+alert(multip)
--- js/entrega2.js
+++ js/entrega2.js
@@ -4,8 +4,24 @@ let user = prompt("Ingrese su nombre");
 
 alert(`Bienvenido ${user} a tu billetera virtual`);
 
-let saldoinicial = 0;
+let saldo = 0;
 let option = 1;
+
+
+/*En el siguiente array (array de objetos) se van agregando las operaciones, con un id, tipo de operacion, monto, y saldo luego de esa operacion*/
+
+class Registro {
+  
+  constructor (fecha, tipo,monto,saldo)
+   { 
+    this.fecha = fecha 
+    this.tipo = tipo;
+    this.monto = monto;
+    this.saldo = saldo;
+   }
+}
+
+const arrayRegistros = []
 
 while (option !== 4) {
 
@@ -14,18 +30,20 @@ while (option !== 4) {
 /*Si elige Agregar Saldo se consulta cuanto agregar, vemos si es un number, agregarmos al saldo y mostramos por pantalla*/
 
   if (option === 1) {
-   agregarSaldo(saldoinicial)
+    let montoingresado = Number(prompt("Qué monto desea ingresar:"));
+    modificarSaldo(montoingresado, option)
   } 
 
  /*Si elige Retirar Saldo se consulta cuanto retira, vemos si es un number, restamos al saldo y mostramos por pantalla*/ 
   
   else if (option === 2) {
-      restarSaldo(saldoinicial)
+      let montoingresado = Number(prompt("Qué monto desea retirar:"));
+      modificarSaldo(montoingresado, option)
 } 
 
 
   else if (option === 3) {
-    alert(`Tu saldo actual es ${saldoinicial} `);
+    alert(`Tu saldo actual es ${saldo} `);
 }
 
   else {
@@ -34,32 +52,50 @@ while (option !== 4) {
   }
 }
 
-function agregarSaldo(saldo) {
-  let montoingresado = Number(prompt("Cuanto dinero ingresas:"));
+console.log(arrayRegistros);
 
-  if (!isNaN(montoingresado)) {
-      saldo = saldoinicial + montoingresado;
-      alert(`Se agregaron ${montoingresado} a tu billetera. \n Tu nuevo saldo es ${saldo} `);
-      saldoinicial = saldo;
-  } else {
-      alert("No ingresaste un numero")
-  } 
-}
 
-function restarSaldo(saldo) {
-  let montoingresado = Number(prompt("Qué monto desea retirar:"));
+/*En esta funcion modificamos el saldo. Sumando o Restando, de acuerdo a la opcion que haya elegido el usuario. Le pasamos como dato el monto, y el tipo de operacion. Agrega el movimiento al array de Registros (llamando a otra funcion)*/ 
 
-  if (!isNaN(montoingresado)) {
+function modificarSaldo(monto, operacion) {
 
-      if (montoingresado > saldo) {
-        alert(`Tu saldo actual es ${saldo}. \n No puedes retirar ${montoingresado} ya que no tienes fondos suficientes`);
-      }  else {
-        saldo = saldo - montoingresado;
-        alert(`Se retiraron ${montoingresado} de tu billetera. \n Tu nuevo saldo es ${saldo} `);
-        saldoinicial = saldo;
+  if (isNaN(monto)) {
+    alert("No ingresaste un numero")
+  }   
+      else {
+
+      if (operacion===1) {
+              saldo = saldo + monto;
+              alert(`Se agregaron ${monto} a tu billetera. \n Tu nuevo saldo es ${saldo} `);
+              let movimiento = "Deposito";
+              agregarRegistro(movimiento, monto, saldo)
       }
-  } else {
-      alert("No ingresaste un numero")
-  }
+      
+        else if (operacion===2) {
+                  if (monto > saldo) {
+                  alert(`Tu saldo actual es ${saldo}. \n No puedes retirar ${monto} ya que no tienes fondos suficientes.`);
+                  }  else {
+                  saldo = saldo - monto;
+                  alert(`Se retiraron ${monto} de tu billetera. \n Tu nuevo saldo es ${saldo} `);
+                  let movimiento = "Retiro";
+                  agregarRegistro(movimiento, monto, saldo);
+
+                  }
+             }
+          } 
 }
+
+
+/*Funcion para ir agregando los registros al array correspondiente*/
+
+function agregarRegistro(operacion, monto, saldo) {
+      let hoy = new Date();
+      let fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + ' ' + hoy.getHours() + ':' + hoy.getMinutes();
+      let nuevoRegistro = new Registro(fecha, operacion,monto,saldo);
+      arrayRegistros.unshift(nuevoRegistro);
+}
+
+console.log(arrayRegistros)
+
+
 
