2c10d7e94cba32150eb17abea633482218401567
TinMon11 <martindanielmondino@gmail.com>
2022-05-23 16:42:21 -0300

    Se agrega lo visto en clase DOM / Interac con HTML

--- js/entrega2.js
+++ js/entrega2.js
@@ -1,8 +1,10 @@
+
 let user = prompt("Ingrese su nombre");
 
-alert(`Bienvenido ${user} a tu billetera virtual`);
+let usuario = document.getElementById("userName");
+usuario.innerText = user;
 
-let saldo = 1700; /*1700 es el saldo con los registros iniciales agregados manualmente en las siguientes lineas*/
+alert(`Bienvenido ${user} a tu billetera virtual`);
 
 /*En el siguiente array (array de objetos) se van agregando las operaciones, con un id, tipo de operacion, monto, y saldo luego de esa operacion*/
 
@@ -27,6 +29,7 @@ const mov4 = new Registro ("26-05-2022 16:20", "Retiro", 201, 800, 1700)
 
 const arrayRegistros = [mov1,mov2,mov3,mov4];
 
+let saldo = 1700; /*1700 es el saldo con los registros iniciales agregados manualmente en las siguientes lineas*/
 
 mostrarMenu()
 
@@ -52,6 +55,7 @@ function mostrarMenu()
             {
             let montoingresado = Number(prompt("Qué monto desea ingresar:"));
             modificarSaldo(montoingresado, opcion);
+            mostrarSaldo(saldo);
             break;
             }
 
@@ -60,6 +64,7 @@ function mostrarMenu()
             {
             let montoingresado = Number(prompt("Qué monto desea retirar:"));
             modificarSaldo(montoingresado, opcion);
+            mostrarSaldo(saldo);
             break;
             }
 
@@ -70,28 +75,32 @@ function mostrarMenu()
             }
 
         case 4: 
-        {
+            {
             alert("Se imprime en consola (por ahora) el registro de movimientos");
             filtrarMovimientos()
             break;
-        }
+            }
 
         case 5: 
-        {   
+            {   
             alert("Se imprime en consola (por ahora) el registro correspondiente a esa factura");
             buscarFactura();
             break;
-        }
+            }
 
         case 6: 
-        {   alert("Gracias por haber usado la SuperBilletera");
+            {   
+            alert("Gracias por haber usado la SuperBilletera");
             break;
-        }
+            }
 
         default:
-           { alert("Opcion Invalida. Elija otra opción");}
+            {
+            alert("Opcion Invalida. Elija otra opción");
+            }
         }
     }
+
 }
 
 /*En esta funcion modificamos el saldo. Sumando o Restando, de acuerdo a la opcion que haya elegido el usuario. Le pasamos como dato el monto, y el tipo de operacion. Agrega el movimiento al array de Registros (llamando a otra funcion)*/ 
@@ -105,12 +114,13 @@ function modificarSaldo(monto, operacion) {
         else {
 
         let factura = Number(prompt("Ingrese el N° de Factura de su Cobro/Gasto:"));
+
         if (operacion===1) {
                 saldo = saldo + monto;
                 alert(`Se agregaron ${monto} a tu billetera. \n Tu nuevo saldo es ${saldo} `);
                 let movimiento = "Deposito";
                 agregarRegistro(movimiento, factura, monto, saldo)
-        }
+                }
         
           else if (operacion===2) {
                     if (monto > saldo) {
@@ -120,13 +130,12 @@ function modificarSaldo(monto, operacion) {
                     alert(`Se retiraron ${monto} de tu billetera. \n Tu nuevo saldo es ${saldo} `);
                     let movimiento = "Retiro";
                     agregarRegistro(movimiento, factura, monto, saldo);
-  
-                    }
-               }
-            } 
+                    }   
+                }   
+            }  
   }
-  
-  
+
+
   /*Funcion para ir agregando los registros al array correspondiente*/
   
   function agregarRegistro(operacion, factura, monto, saldo) {
@@ -167,10 +176,17 @@ if (isNaN(buscada))
  {
     alert("No ingresaste un numero") 
  } else 
-    {
+    { 
     let facturaBuscada = arrayRegistros.find ((fc) => fc.factura === buscada);
     console.log(facturaBuscada);
     }
 
 }
-  
+
+
+
+function mostrarSaldo(saldo) 
+    {
+        let nuevoSaldo = document.getElementById("saldo")
+        nuevoSaldo.innerText = saldo;
+    }
